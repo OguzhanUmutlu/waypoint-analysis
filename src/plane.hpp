d9b68b937e9ffb8b0fe4b92aafc91eed102d353c
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2025-11-29 14:04:41 +0300

    format on plane

--- src/plane.hpp
+++ src/plane.hpp
@@ -5,10 +5,10 @@
 constexpr Vector2 Wind{mps2{15}, mps2{0}};
 static Rect Geofence{0_m, 0_m, ScreenSize.x * px2m, ScreenSize.y * px2m};
 
-struct Plane
-{
+struct Plane {
     Sprite stable_sprite;
     Sprite moving_sprite;
+
     Vector2<m> position{};
     rad yaw{};
     rad roll{};
@@ -21,17 +21,16 @@ struct Plane
     static constexpr rad roll_damping{0.3_deg};
     static constexpr auto wing_span = 1.5_m;
     static constexpr rad max_roll{30_deg};
-    static constexpr auto roll_yaw_factor = 1.5 / 1_s;
+    static constexpr auto roll_yaw_factor  = 1.5 / 1_s;
     static constexpr auto thrust_magnitude = 10.0_m / 1_s / 1_s;
-    static constexpr auto drag_factor = thrust_magnitude / (mps{10} * mps{10}) * mps{1};
+    static constexpr auto drag_factor      = thrust_magnitude / (mps{10} * mps{10}) * mps{1};
 
     const sf::Vector2<float> sprite_initial_scale{
         static_cast<float>((wing_span * m2px / stable_sprite.getLocalBounds().size.x).value),
         static_cast<float>((wing_span * m2px / stable_sprite.getLocalBounds().size.y).value)
     };
 
-    rad_ps roll_speed() const
-    {
+    rad_ps roll_speed() const {
         auto cruise_velocity = (velocity - Wind * 1_s).length(); // m/s
 
         // p_ss
@@ -42,12 +41,10 @@ struct Plane
     }
 
     Plane() : stable_sprite(loadTexture(0, "plane_stable.png")),
-              moving_sprite(loadTexture(1, "plane_moving.png"))
-    {
+              moving_sprite(loadTexture(1, "plane_moving.png")) {
         auto stable_bound = stable_sprite.getLocalBounds();
         auto moving_bound = moving_sprite.getLocalBounds();
-        if (stable_bound != moving_bound)
-        {
+        if (stable_bound != moving_bound) {
             std::cout << stable_bound.size.x << std::endl;
             std::cout << stable_bound.size.y << std::endl;
             std::cout << moving_bound.size.x << std::endl;
@@ -70,12 +67,9 @@ private:
     static bool texture_loaded[2];
     static Texture textures[2];
 
-    static Texture& loadTexture(int index, std::string_view path)
-    {
-        if (!texture_loaded[index])
-        {
-            if (!textures[index].loadFromFile(path))
-            {
+    static Texture& loadTexture(int index, std::string_view path) {
+        if (!texture_loaded[index]) {
+            if (!textures[index].loadFromFile(path)) {
                 throw runtime_error("Failed to load plane texture");
             }
             texture_loaded[index] = true;
